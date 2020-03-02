2ca9385a51f4aacd13fd1488d8468e58b920ed8c
Sam Jones <sam.jones494@googlemail.com>
2020-03-02 13:25:10 +0000

    initial commit

---
+++ src/main/kotlin/com/example/pocketcloneapi/PocketCloneApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.pocketcloneapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocketCloneApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PocketCloneApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/pocketcloneapi/PocketCloneApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.pocketcloneapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PocketCloneApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

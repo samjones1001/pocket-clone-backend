f174c9854d70acb55977c8c2a61f7ee4a48497d2
Sam Jones <sam.jones494@googlemail.com>
2020-03-05 10:13:29 +0000

    extract and save document titles

--- src/main/kotlin/com/example/pocketcloneapi/articles/ArticleService.kt
+++ src/main/kotlin/com/example/pocketcloneapi/articles/ArticleService.kt
@@ -2,16 +2,34 @@ package com.example.pocketcloneapi.articles
 
 import com.example.pocketcloneapi.articles.persistance.Article
 import com.example.pocketcloneapi.articles.persistance.ArticlesRepository
+import com.example.pocketcloneapi.articles.web.ArticleUpload
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.jsoup.Jsoup
+import org.springframework.stereotype.Component
 
 @Service
-class ArticleService(@Autowired private val articlesRepository: ArticlesRepository) {
+class ArticleService(
+    @Autowired private val articlesRepository: ArticlesRepository,
+    @Autowired private val jsoup: JsoupConnection
+) {
 
     fun findAll(): List<Article> = articlesRepository.findAll()
 
-    fun create(article: Article): Article = articlesRepository.save(article)
+    fun create(articleUpload: ArticleUpload): Article {
+        val article = Article(articleUpload.url, getArticleTitle(articleUpload.url))
+        return articlesRepository.save(article)
+    }
 
     fun delete(id: Long): Unit = articlesRepository.deleteById(id)
 
+    private fun getArticleTitle(url: String): String {
+        return jsoup.getTitle(url)
+    }
+
+}
+
+@Component
+class JsoupConnection() {
+    fun getTitle(url: String): String = Jsoup.connect(url).get().title()
 }
